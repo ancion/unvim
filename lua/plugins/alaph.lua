1ffe80b56c5aba2672a3309d0f063e97cc5de448
ancion <ancion@github.com>
2023-07-16 23:12:26 +0800

    delete unused function

--- lua/plugins/alaph.lua
+++ lua/plugins/alaph.lua
@@ -26,30 +26,6 @@ local header = {
   "                                               .....                                                                                      ",
 }
 
-local function max_len_line(lines)
-  local max_len = 0
-
-  for _, line in ipairs(lines) do
-    local line_len = line:len()
-    if line_len > max_len then
-      max_len = line_len
-    end
-  end
-
-  return max_len
-end
-local function align_center(container, lines, alignment)
-  local output = {}
-  local max_len = max_len_line(lines)
-
-  for _, line in ipairs(lines) do
-    local padding = string.rep(" ", (math.max(container.width, max_len) - line:len()) * alignment)
-    table.insert(output, padding .. line)
-  end
-
-  return output
-end
-
 local plugin_num = #vim.fn.globpath(vim.call("stdpath", "data") .. "/site/pack/lazy/opt", "*", 0, 1)
 local nvim_version = 10.0
 local footer = {
